a641efc50324dfd6ddf1e9ea5a381f5713d23c0a
David John <davidkjohn01@gmail.com>
2020-04-25 14:15:29 +0530

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,28 @@
-var debug = process.env.NODE_ENV !== "production";
+var debug = process.env.NODE_ENV !== 'production';
 var webpack = require('webpack');
 var module_dir = `${__dirname}/node_modules`;
 const path = require('path');
 
-
 module.exports = {
-    target: "node",
-    entry: './server.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js',
-    },
-    module: {
-        rules: [{
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            options: {
-                presets: [
-                    [
-                        "@babel/preset-env",
-                    ]
-                ]
-            }
-        }]
-    },
-    resolveLoader: {
-        modules: [
-            __dirname + '/node_modules'
-        ]
-    }
-}
-
+  target: 'node',
+  entry: './server/server.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [['@babel/preset-env']],
+        },
+      },
+    ],
+  },
+  resolveLoader: {
+    modules: [__dirname + '/node_modules'],
+  },
+};
