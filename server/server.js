3c9fa2f911dcf18abbfa00b2ec64b96cff37d61f
David John <davidkjohn01@gmail.com>
2020-04-28 12:38:00 +0530

    refactored to server/

---
+++ server/server.js
@@ -0,0 +1,34 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const os = require('os');
+const mongoose = require('mongoose');
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} else {
+  mongoose.connect(process.env.TEST_DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
+
+const db = mongoose.connection;
+db.on('error', (error) => console.log('Error from database', error));
+db.once('open', () => console.log('Connected to database'));
+
+app.use(express.json());
+
+const articleRoute = require('./routes/articles');
+app.use('/articles', articleRoute);
+
+const server = app.listen(process.env.PORT || 2500, () => {
+  const host = os.hostname();
+  console.log('Server Started at ', host, ':', server.address().port);
+});
+
+module.exports = server;
